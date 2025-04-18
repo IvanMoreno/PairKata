34281b9aef9ff990d882f325d923cf549b79be17
rnogue <rogernogueb@gmail.com>
2025-04-18 11:24:29 +0200

    doc: filled the initial requirements

--- PairKata.Tests/UnitTest1.cs
+++ PairKata.Tests/UnitTest1.cs
@@ -1,5 +1,24 @@
 namespace PairKata.Tests;
+/*
+ link: https://www.codurance.com/katas/bags
+    - we need a backpack that can have items stored.
+    - the backpack has capacity of 8
+    - we need bags that can have items stored.
+    - bags have capacity of 4
+    - items are added to the backpack
+    - If the backpack happens to be full, the item is added to the next available bag.
+    - There is a Sorting spell
+    - Each bag can have a category, during the sorting spell only the items that match the category can be placed there.
+         {
+            "clothes": ["Leather", "Linen", "Silk", "Wool"],
+            "herbs": ["Cherry Blossom", "Marigold", "Rose", "Seaweed"],
+            "metals": ["Copper", "Gold", "Iron", "Silver"],
+            "weapons": ["Axe", "Dagger", "Mace", "Sword"]
+          }
+    - Sorting spell orders alphabetically
+    - When a bag with category is filled during the Sorting Spell, the backpack gets filled with the rest of the items.
 
+ */
 public class Tests
 {
     [Test]
