b90d81e9a040962e68a6eb3e938ff19203fc3f4d
rnogue <rogernogueb@gmail.com>
2025-04-18 12:40:44 +0200

    doc: completed step marked as such

--- PairKata.Tests/UnitTest1.cs
+++ PairKata.Tests/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace PairKata.Tests;
     - [x] we need bags that can have items stored.
     - [x] bags have capacity of 4
     - [x] items are added to the backpack
-    - If the backpack happens to be full, the item is added to the next available bag.
+    - [x] If the backpack happens to be full, the item is added to the next available bag.
     - Items are not just strings, they have types.
     - There is a Sorting spell
     - Each bag can have a category, during the sorting spell only the items that match the category can be placed there.
